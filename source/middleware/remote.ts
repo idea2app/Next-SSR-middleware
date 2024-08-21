41cfe9b6b23d07c2e238b23409ba5a9043e409f4
TechQuery <shiy2008@gmail.com>
2024-08-22 03:33:52 +0800

    [add] Scope types of GitHub OAuth 2 [add] GitHub PR badges [optimize] support Custom Base URLs for GitHub OAuth 2 [optimize] update Upstream packages & support NPM provenance

--- source/middleware/remote.ts
+++ source/middleware/remote.ts
@@ -68,17 +68,36 @@ export function oauth2Signer<I extends DataObject, O extends DataObject = {}>({
     };
 }
 
+export type GitHubAPIOperation = 'admin' | 'write' | 'read' | 'delete';
+
+export type GitHubOAuthScope =
+    | `repo${'' | ':status' | '_deployment' | ':invite'}`
+    | 'public_repo'
+    | 'security_events'
+    | `${Exclude<GitHubAPIOperation, 'delete'>}:${'repo_hook' | 'org' | 'public_key' | 'gpg_key'}`
+    | 'admin:org_hook'
+    | 'gist'
+    | 'notifications'
+    | `user${'' | ':email' | ':follow'}`
+    | `read:${'user' | 'project'}`
+    | 'project'
+    | 'delete_repo'
+    | `${Exclude<GitHubAPIOperation, 'admin'>}:packages`
+    | 'codespace'
+    | 'workflow';
 /**
  * @see {@link https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#web-application-flow}
  */
 export interface GitHubOAuthOption
-    extends Record<'client_id' | 'client_secret', string> {
-    scopes?: string[];
-    login?: string;
+    extends Partial<Record<'rootBaseURL' | 'apiBaseURL' | 'login', string>>,
+        Record<'client_id' | 'client_secret', string> {
+    scopes?: GitHubOAuthScope[];
     allow_signup?: boolean;
 }
 
 export function githubOAuth2<I extends DataObject, O extends DataObject = {}>({
+    rootBaseURL = 'https://github.com',
+    apiBaseURL = 'https://api.github.com',
     client_id,
     client_secret,
     scopes,
@@ -86,15 +105,18 @@ export function githubOAuth2<I extends DataObject, O extends DataObject = {}>({
 }: GitHubOAuthOption) {
     return oauth2Signer<I, O>({
         signInURL: redirect_uri =>
-            `https://github.com/login/oauth/authorize?${buildURLData({
-                client_id,
-                scope: scopes?.join(' '),
-                allow_signup,
-                redirect_uri
-            })}`,
+            new URL(
+                `login/oauth/authorize?${buildURLData({
+                    client_id,
+                    scope: scopes?.join(' '),
+                    allow_signup,
+                    redirect_uri
+                })}`,
+                rootBaseURL
+            ) + '',
         accessToken: async ({ code }) => {
             const response = await fetch(
-                'https://github.com/login/oauth/access_token',
+                new URL('login/oauth/access_token', rootBaseURL),
                 {
                     method: 'POST',
                     headers: {
@@ -111,7 +133,7 @@ export function githubOAuth2<I extends DataObject, O extends DataObject = {}>({
             return access_token;
         },
         userProfile: async token => {
-            const response = await fetch('https://api.github.com/user', {
+            const response = await fetch(new URL('user', apiBaseURL), {
                 headers: {
                     Authorization: `Bearer ${token}`,
                     Accept: 'application/json'

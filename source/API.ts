1dde9fb7fe526aaee4bd89006d853c02a0a56fca
TechQuery <shiy2008@gmail.com>
2025-04-26 18:23:46 +0800

    [optimize] support Koa option overrides [fix] Peer & Optional dependencies

--- source/API.ts
+++ source/API.ts
@@ -1,8 +1,26 @@
-import Router, { RouterOptions } from '@koa/router';
+import BodyParser from '@koa/bodyparser';
+import Router, { RouterOptions, RouterParamContext } from '@koa/router';
 import Koa, { Middleware } from 'koa';
 
-export function withKoa<S, T>(...middlewares: Middleware<S, T>[]) {
-    const app = new Koa();
+export type KoaOption = ConstructorParameters<typeof Koa>[0];
+
+export type KoaCallback = ReturnType<(typeof Koa)['prototype']['callback']>;
+
+export function withKoa<S, C>(...middlewares: Middleware<S, C>[]): KoaCallback;
+export function withKoa<S, C>(
+    option: KoaOption,
+    ...middlewares: Middleware<S, C>[]
+): KoaCallback;
+export function withKoa<S, C>(...parameters: any[]) {
+    let option: KoaOption | undefined;
+    let middlewares: Middleware<S, C>[] = [];
+
+    if (typeof parameters[0] === 'function') {
+        option = undefined;
+        middlewares = parameters;
+    } else [option, ...middlewares] = parameters;
+
+    const app = new Koa(option).use(BodyParser());
 
     for (const middleware of middlewares) app.use(middleware);
 
@@ -15,8 +33,35 @@ export const pageApiRouteOf = (path: string) =>
         ?.split('/[')[0]
         ?.replace(/\/index$/, '');
 
-export const createRouter = (moduleURI: string, option?: RouterOptions) =>
-    new Router({ ...option, prefix: pageApiRouteOf(moduleURI) });
+export const createRouter = <S, C extends RouterParamContext<S>>(
+    moduleURI: string,
+    option?: RouterOptions
+) => new Router<S, C>({ ...option, prefix: pageApiRouteOf(moduleURI) });
 
-export const withKoaRouter = (router: Router, ...middlewares: Middleware[]) =>
-    withKoa(router.routes(), router.allowedMethods(), ...middlewares);
+export function withKoaRouter<S, C extends RouterParamContext<S>>(
+    router: Router<S, C>,
+    ...middlewares: Middleware<S, C>[]
+): KoaCallback;
+export function withKoaRouter<S, C extends RouterParamContext<S>>(
+    option: KoaOption,
+    router: Router<S, C>,
+    ...middlewares: Middleware<S, C>[]
+): KoaCallback;
+export function withKoaRouter<S, C extends RouterParamContext<S>>(
+    ...parameters: any[]
+) {
+    let option: KoaOption | undefined;
+    let router: Router<S, C>;
+    let middlewares: Middleware<S, C>[] = [];
+
+    if (typeof parameters[1] === 'function')
+        [router, ...middlewares] = parameters;
+    else [option, router, ...middlewares] = parameters;
+
+    return withKoa(
+        option,
+        router.routes(),
+        router.allowedMethods(),
+        ...middlewares
+    );
+}
